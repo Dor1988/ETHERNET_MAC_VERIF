55fb068093e8f96318d7f32fc4bdb2f574aefe3f
Dor1988 <92331655+Dor1988@users.noreply.github.com>
2024-06-26 16:59:25 +0300

    Update wb_proc_seq_lib.sv

--- wb_proc_seq_lib.sv
+++ wb_proc_seq_lib.sv
@@ -50,5 +50,41 @@ endtask
 endclass
 
 
+class wb_reg_write_read_reg_model_seq extends wb_proc_base_seq;
 
+uvm_reg mac_regs[$]; // queue of uvm_reg
+uvm_reg_data_t ref_data[$]; // will be used for compare purpose
 
+`uvm_object_utils(wb_reg_write_read_reg_model_seq)
+`NEW_OBJ 
+
+task body();
+mac_reg_block	mac_rm;
+int [31:0] errors; // used for counting errors
+super.body;
+uvm_resource_db#(mac_reg_block)::read_by_name("GLOBAL", "MAC_RM", mac_rm, this);
+mac_rm.get_registers(mac_regs); // getting all register handles in mac_regs queue
+
+//set errors to 0
+errors =0;
+
+	mac_regs.shuffle();
+	foreach(mac_regs[i]) begin
+		if (this.randomize()) begin
+			`uvm_error("body", "randomization error")
+		end
+		mac_regs[i].write(status, data, .parent(this)); // performing write 
+	end
+	mac_regs.shuffle();
+	foreach(mac_regs[i]) begin
+		ref_data=mac_regs[i].get(); // the data from the register model
+		mac_regs[i].read(status,data, .parent(this));// get the register value from DUT register
+		if(ref_data!=data) begin
+			``uvm_error("REG_TEST_SEQ:", $sformatf("get/read: Read error for %s: Expected: %0h Actual: %0h", mac_regs[i].get_name(), ref_data,data))
+			errors++;
+		end
+	end
+end
+
+endtask: body
+endclass
