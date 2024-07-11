d9b8d33d468bc4e96edcae3ee20fc120fecdc5b8
Dor1988 <92331655+Dor1988@users.noreply.github.com>
2024-07-11 19:45:51 +0300

    Update phy_rx_agent.sv

--- phy_rx_agent.sv
+++ phy_rx_agent.sv
@@ -1,10 +1,11 @@
 class phy_rx_agent extends uvm_agent;
-
+phy_rx_drv drv;
 `uvm_component_utils(phy_rx_agent)
 `NEW_COMP
 
 function void build_phase(uvm_phase phase);
 	super.build_phase(phase);
+	drv=phy_rx_drv::type_id::create("drv",this);
 endfunction
 
 endclass
