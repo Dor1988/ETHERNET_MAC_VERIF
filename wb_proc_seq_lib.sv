87fdfb3e86f8dd7321cd756ad0608ec7b51aed6e
Dor1988 <92331655+Dor1988@users.noreply.github.com>
2024-07-11 19:44:50 +0300

    Update wb_proc_seq_lib.sv

--- wb_proc_seq_lib.sv
+++ wb_proc_seq_lib.sv
@@ -65,6 +65,7 @@ uvm_reg mac_regs[$]; // queue of uvm_reg
 uvm_reg_data_t ref_data; // will be used for compare purpose
 
 rand uvm_reg_data_t data; // will be used for compare purpose
+uvm_reg_data_t miicommand_wr_data;
 uvm_status_e status;
 
 `uvm_object_utils(wb_reg_wr_rd_rm_seq)
@@ -72,6 +73,8 @@ uvm_status_e status;
 
 task body();
 mac_reg_block	reg_block;
+string reg_name;
+uvm_reg_addr_t addr;
 int errors; // used for counting errors
 super.body();
 uvm_resource_db#(mac_reg_block)::read_by_name("GLOBAL", "MAC_RM", reg_block, this);
@@ -79,14 +82,41 @@ reg_block.get_registers(mac_regs); // getting all register handles in mac_regs q
 //mac_regs is a queue of 21 registers handles
 
 	errors =0;//set errors to 0
-
+	
+repeat (10) begin
 	mac_regs.shuffle(); // this is shuffling the order of the register in the queue of the 21 registers 
 	foreach(mac_regs[i]) begin
 		if (!this.randomize()) begin
 			`uvm_error("body", "randomization error")
 		end
+		// addr=mac_regs[i].get_address();
+		// if (addr==32'hb)begin
+			// data[0]=0;
+		// end		
+		reg_name=mac_regs[i].get_name();
+		
+		if (reg_name=="miicommand")begin
+		//	data[1:0]= 2'b11;
+			miicommand_wr_data=data;
+		end
+		
+		if (reg_name=="txbdnum")begin
+			if (data> 8'h80) data=8'h7F;
+		end
+		
+	//	if (data[0]==1) // we should make sure that miistatus[1][2] both should be made 1
+		
+		//update the register model, so that miistatus[2:1]= 2'b11
+		
+		
+		
 		mac_regs[i].write(status, data, .parent(this)); // performing write of random data into the register
 		// uvm_reg_rw kind of object => adapter
+	end
+	
+	//whenever miicommand_wr_data[0] is made 1 => Nvalid_stat and busy_stat to go high
+	if (miicommand_wr_data[0] == 1 ) begin
+		reg_block.miistatus.predict(3'b110); // updating the register model value for miistatus, note that the register model value is the expected value and the Dut register value is the Actual value
 	end
 	mac_regs.shuffle();
 
@@ -98,7 +128,7 @@ reg_block.get_registers(mac_regs); // getting all register handles in mac_regs q
 			errors++;
 		end
 	end
-
+end // 10 time repeat loop
 endtask: body
 endclass
 
@@ -154,4 +184,125 @@ endtask: body
 
 endclass
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//sequence for wr_rd_test frontdoor (with regmodel)
+class wb_reg_read_rm_seq extends wb_proc_base_seq;
+
+uvm_reg mac_regs[$]; // queue of uvm_reg
+uvm_reg_data_t ref_data; // will be used for compare purpose
+
+rand uvm_reg_data_t data; // will be used for compare purpose
+uvm_reg_data_t miicommand_wr_data;
+uvm_status_e status;
+
+`uvm_object_utils(wb_reg_read_rm_seq)
+`NEW_OBJ 
+
+task body();
+mac_reg_block	reg_block;
+string reg_name;
+uvm_reg_addr_t addr;
+int errors; // used for counting errors
+super.body();
+uvm_resource_db#(mac_reg_block)::read_by_name("GLOBAL", "MAC_RM", reg_block, this);
+reg_block.get_registers(mac_regs); // getting all register handles in mac_regs queue
+//mac_regs is a queue of 21 registers handles
+
+	errors =0;//set errors to 0
+	
+repeat (10) begin
+	mac_regs.shuffle();
+	foreach(mac_regs[i]) begin
+		ref_data=mac_regs[i].get_reset(); // getting the reset value stored in the register model, reset value as per spec
+		mac_regs[i].read(status,data, .parent(this));// get the Actual reset value from DUT register
+		if(ref_data!=data) begin
+			`uvm_error("REG_TEST_SEQ:", $sformatf("Read error for %s: Expected: %0h Actual: %0h", mac_regs[i].get_name(), ref_data,data))
+			errors++;
+		end
+	end
+end //   repeat loop
+endtask: body
+endclass
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// full duplex sequence
+class mac_10mbps_fd_tx_seq extends wb_proc_base_seq;
+
+uvm_status_e status;
+rand uvm_reg_data_t moder_data; // will be used for compare purpose
+
+`uvm_object_utils(mac_10mbps_fd_tx_seq)
+`NEW_OBJ 
+
+task body();
+mac_reg_block	reg_block;
+bit [31:0] data_t;
+string reg_name;
+uvm_reg_addr_t addr;
+int errors; // used for counting errors
+// super.body();
+//we need register model handle
+uvm_resource_db#(mac_reg_block)::read_by_name("GLOBAL", "MAC_RM", reg_block, this);
+//`uvm_do_with(req,{req.addr==13'h0; req.data=data_t;req.write==1;})
+
+
+//////////////////////////////////////////////////////////
+// Register programming  :
+//////////////////////////////////////////////////////////
+
+moder_data[16]=0; // RECSMALL
+moder_data[15]=0;// PADEN
+moder_data[14]=0;// HUGEN
+moder_data[13]=0;// CRCEN
+moder_data[12]=0;// DLYCRCEN
+moder_data[11]=0;// RSVD
+moder_data[10]=1;// FD mode
+moder_data[9]=0;// EXDFREN
+moder_data[8]=0; // NOBCKOF
+moder_data[7]=0; // LOOPBCK
+moder_data[6]=0; // IFG
+moder_data[5]=0; // PRO – Promiscuous
+moder_data[4]=0; // IAM – Individual Address Mode
+moder_data[3]=0; // BRO – Broadcast Address
+moder_data[2]=0; // NOPRE – No Preamble
+moder_data[1]=0; // TXEN – Transmit Enable. TxEN=o for now 
+moder_data[0]=0; // RXEN – Receive Enable
+
+// we are performing write to the moder register by using register model
+reg_block.moder.write(status,moder_data); // configure the Mac to work in FULL DUPLEX MODE 
+/////////////////////////////////
+// Finish Register programming 
+/////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////
+// Start Loading the TX_BD(transmit buffer descriptors) :
+//////////////////////////////////////////////////////////
+
+// A total of 512 bytes will be transmitted
+// we can't load these tx_bd using register model because my register model only contain the 21 registers and not the buffer descriptors
+// so we load it using `uvm_do_with
+
+// data_t= {[31:16] it is LEN length,[15] RD,[14] IRQ, [13]WR, [12]PAD,[11]CRC en,[10:9] reserved, and more... }
+data_t= {16'h200,1'b1,1'b1,1'b1,1'b0,1'b0,2'b0,1'b0,4'b0,1'b0,1'b0,1'b0,1'b0}; // 32 bits that are 4 bytes
+// sending 8 bytes so we need two 31 bits(4 bytes) wisbone transactions
+// 400 is an actual addrees, but do'nt drive 400, drive only 100
+`uvm_do_with(req,{req.addr==13'h100; req.data==data_t; req.wr_rd==1'b1;}) // write to the descriptor data of 4 bytes : {[31:16] LEN, [15] RD,[14] IRQ,, [13]WR, [12]PAD,[11]CRC,[10:9] reserved,[8]UR,[7:4] RTRY,[3] RL, LC [2], [1] DF, [0] cs}
+data_t=32'h1000_0000; // this will tell us from whice address at the External memory the MAC's DMA controller should read, the 512 bytes be present From this address (32'h1000_0000) onwards
+// 404=> 101
+`uvm_do_with(req,{req.addr==13'h101; req.data==data_t; req.wr_rd==1'b1;}) //
+
+///////////////////////////////////////////////////////////
+// Finish of Loading the TX_BD(transmit buffer descriptors)
+///////////////////////////////////////////////////////////
+
+// rest all fields are same, only write TXEN=1
+moder_data[1]= 1; // TXEN=1
+reg_block.moder.write(status, moder_data);
+
+endtask: body
+endclass
