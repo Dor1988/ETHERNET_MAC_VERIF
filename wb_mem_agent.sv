be03e7b15f7019771312cdc75029fea2c6ef61a3
Dor1988 <92331655+Dor1988@users.noreply.github.com>
2024-07-11 19:47:38 +0300

    Update wb_mem_agent.sv

--- wb_mem_agent.sv
+++ wb_mem_agent.sv
@@ -1,10 +1,11 @@
 class wb_mem_agent extends uvm_agent;
-
+memory memory_i;
 `uvm_component_utils(wb_mem_agent)
 `NEW_COMP
 
 function void build_phase(uvm_phase phase);
 	super.build_phase(phase);
+	memory_i=memory::type_id::create("memory_i",this);
 endfunction
 
 endclass
