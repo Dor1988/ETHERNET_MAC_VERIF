8c4d24d502daf39dea553609db18d32d301d6735
Dor1988 <92331655+Dor1988@users.noreply.github.com>
2024-07-18 15:19:50 +0300

    Update wb_mem_intf.sv

--- verif/memory/wb_mem_intf.sv
+++ verif/memory/wb_mem_intf.sv
@@ -15,6 +15,22 @@ bit   [29:0]  m_wb_adr_tmp;
 bit   [2:0]   m_wb_cti_o;   // Cycle Type Identifier
 bit   [1:0]   m_wb_bte_o;   // Burst Type Extension
 
+clocking slave_cb@(posedge wb_clk);
+default input #0 output #1;
+input wb_rst;
+// The directions are relative to the designm so output for the design it is an input to memory
+input  m_wb_adr_o;
+input	m_wb_sel_o;
+input m_wb_we_o;
+output m_wb_dat_i;
+input m_wb_dat_o;
+input m_wb_cyc_o;
+input m_wb_stb_o;
+output m_wb_ack_i;
+output m_wb_err_i;
+input m_wb_cti_o;   
+input m_wb_bte_o;
 
+endclocking
 
 endinterface
